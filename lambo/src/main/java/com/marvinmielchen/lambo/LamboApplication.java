2cde7e715373e0c6366f62be946368fc519ad8a9
0mielche <marvin.mielchen@studium.uni-hamburg.de>
2023-12-05 12:36:56 +0100

    init springboot application

---
+++ lambo/src/main/java/com/marvinmielchen/lambo/LamboApplication.java
@@ -0,0 +1,13 @@
+package com.marvinmielchen.lambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LamboApplication.class, args);
+    }
+
+}
---
+++ lambo/src/test/java/com/marvinmielchen/lambo/LamboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvinmielchen.lambo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
