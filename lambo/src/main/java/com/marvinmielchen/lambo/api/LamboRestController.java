6c1a1a73d2314de4bc0386089eeea97b4246203b
MarvinMielchen <mielchen.2001@gmail.com>
2023-12-18 23:58:31 +0100

    first working api endpoints

--- lambo/src/main/java/com/marvinmielchen/lambo/api/LamboRestController.java
+++ lambo/src/main/java/com/marvinmielchen/lambo/api/LamboRestController.java
@@ -2,10 +2,7 @@ package com.marvinmielchen.lambo.api;
 
 
 import com.marvinmielchen.lambo.Lambo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,19 +10,14 @@ import java.util.List;
 @RequestMapping("/api")
 public class LamboRestController {
 
-    @GetMapping(value = "syntax_check", produces = "application/json")
-    public List<String> syntaxCheck(@RequestParam("sourcecode") String sourcecode) {
+    @GetMapping(value = "syntax", produces = "application/json", consumes = "text/plain")
+    public List<String> syntaxCheck(@RequestBody String sourcecode) {
         return Lambo.syntaxCheck(sourcecode);
     }
 
-    @GetMapping(value = "beta_reduction", produces = "application/json")
-    public List<String> betaReduction(@RequestParam("sourcecode") String sourcecode) {
+    @GetMapping(value = "reduction", produces = "application/json", consumes = "text/plain")
+    public List<String> betaReduction(@RequestBody String sourcecode) {
         return Lambo.betaReduction(sourcecode);
-    }
-
-    @GetMapping(value = "statement_substitution", produces = "application/json")
-    public String statementSubstitution(@RequestParam("sourcecode") String sourcecode) {
-        return "{\"status\": \"ok\"}";
     }
 
     //status endpoint
