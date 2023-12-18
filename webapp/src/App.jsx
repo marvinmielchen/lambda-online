9efc800a6d15a699ed8bb953bd39c4bb10190297
MarvinMielchen <mielchen.2001@gmail.com>
2023-12-19 00:36:27 +0100

    experimenting with ui

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,25 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Editor from "@monaco-editor/react"
 import './App.css'
 
 
-function App(){
-    const [count, setCount] = useState(0)
-
-    // it will take up the full width / height of its container
-    //editor of any size
-
+function App() {
+    const [count, setCount] = useState(0);
 
     return (
         <div className="App">
-            <Editor
-                height="100vh"
-                width="100%"
-                theme="vs-dark"
-                defaultLanguage="text"
-            />
+            <div className="sidebar">
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                </ul>
+            </div>
+            <div className="content">
+                <button>Substitute Definitions</button>
+                <Editor
+                    height="100vh"
+                    width="100%"
+                    theme="vs-dark"
+                    defaultLanguage="text"
+                />
+            </div>
         </div>
-    )
+    );
 }
+
 export default App
