5ce55cd908fc462667a399dd01daf7af51d0eaf5
MarvinMielchen <mielchen.2001@gmail.com>
2023-12-18 18:37:06 +0100

    new website configuration

---
+++ webapp/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Editor from "@monaco-editor/react"
+import './App.css'
+
+
+function App(){
+    const [count, setCount] = useState(0)
+
+    // it will take up the full width / height of its container
+    //editor of any size
+
+
+    return (
+        <div className="App">
+            <Editor
+                height="100vh"
+                width="100%"
+                theme="vs-dark"
+                defaultLanguage="javascript"
+            />
+        </div>
+    )
+}
+export default App
