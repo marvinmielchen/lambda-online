f56473e932f76812e647201ddda3a7a1b0e40aa7
MarvinMielchen <mielchen.2001@gmail.com>
2023-12-19 22:48:52 +0100

    cors configs

--- lambo/src/main/java/com/marvinmielchen/lambo/api/LamboRestController.java
+++ lambo/src/main/java/com/marvinmielchen/lambo/api/LamboRestController.java
@@ -10,17 +10,19 @@ import java.util.List;
 @RequestMapping("/api")
 public class LamboRestController {
 
-    @GetMapping(value = "syntax", produces = "application/json", consumes = "text/plain")
+    @CrossOrigin(origins = "*")
+    @PostMapping(value = "syntax", produces = "application/json", consumes = "text/plain")
     public List<String> syntaxCheck(@RequestBody String sourcecode) {
         return Lambo.syntaxCheck(sourcecode);
     }
 
-    @GetMapping(value = "reduction", produces = "application/json", consumes = "text/plain")
+    @CrossOrigin(origins = "*")
+    @PostMapping(value = "reduction", produces = "application/json", consumes = "text/plain")
     public List<String> betaReduction(@RequestBody String sourcecode) {
         return Lambo.betaReduction(sourcecode);
     }
 
-    //status endpoint
+    @CrossOrigin(origins = "*")
     @GetMapping(value = "status", produces = "application/json")
     public String status() {
         return "{\"status\": \"ok\"}";
