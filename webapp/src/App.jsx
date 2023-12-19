238170e3b88e39115d80fa7661b4dfbdaa699d84
MarvinMielchen <mielchen.2001@gmail.com>
2023-12-19 22:13:56 +0100

    first good-looking ui

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
 import Editor from "@monaco-editor/react"
 import './App.css'
 
+import { useState, useEffect } from 'react'
+import {text} from './examplecode.js'
 
 function App() {
-    const [content, setContent] = useState("asdfasdf");
+    const [content, setContent] = useState(() => {
+        const content = window.sessionStorage.getItem("content")
+        return content || text
+    })
 
     //reload on resize
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            window.location.reload()
+        })
+    }, [])
 
+    useEffect(() => {
+        window.sessionStorage.setItem("content", String(content))
+    }, [content])
+
+    useEffect(() => {
+        const content = window.sessionStorage.getItem("content")
+        setContent(content)
+    }, [])
+
+    const handleEditorChange = (value) => {
+        setContent(value)
+    }
+
+    const handleReset = () => {
+        console.log("reset")
+        setContent(text)
+    }
 
     return (
         <div className="App">
@@ -20,15 +46,17 @@ function App() {
             </div>
             <div className="content">
                 <div className="toolbar">
-                    <button>Definitionen einsetzen</button>
+                    <button>Definitionen-Substitution</button>
                     <button>Beta-Reduktion</button>
+                    <button onClick={handleReset}>Zurücksetzen</button>
                 </div>
                 <Editor
                     height="100vh"
                     width="100%"
                     theme="vs-dark"
                     defaultLanguage="text"
-                    defaultValue={content}
+                    value={String(content)}
+                    onChange={handleEditorChange}
                 />
             </div>
         </div>
---
+++ webapp/src/examplecode.js
@@ -0,0 +1,15 @@
+//example code for the editor
+export const text =
+`
+
+//this is a comment
+
+def true(t f){
+    t
+}
+
+def false(t f){
+    f
+}
+
+`
