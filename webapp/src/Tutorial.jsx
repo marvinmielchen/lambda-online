5eeefefdfb27ded5eb99b7f667ff1b9c50f6f119
MarvinMielchen <mielchen.2001@gmail.com>
2024-01-09 12:43:42 +0100

    update

--- webapp/src/Tutorial.jsx
+++ webapp/src/Tutorial.jsx
@@ -1,18 +1,41 @@
 import "./Tutorial.css";
+import Markdown from "react-markdown";
+
+
+const markdown =
+`
+# Lambo
+Willkommen im Online Interpreter der Programmiersprache Lambo. Lambo ist eine minimalistische Sprache, die auf den Prinzipien des Lambda-Kalküls basiert.
+
+Diese Website ist das Ergebnis einer Seminararbeit an der Universität Hamburg.
+
+Beginnen Sie mit dem Schreiben von Lambo-Code im Hauptfenster und nutzen Sie die Auswertungsfunktionen, um das Ergebnis zu sehen. Experimentieren Sie mit verschiedenen Funktionen und Parametern, um ein tieferes Verständnis des Lambda-Kalküls zu erlangen.
+
+## Syntax
+Die Syntax von Lambo ist sehr einfach. Es gibt nur drei Arten von Ausdrücken:
+
+- Variablen
+- Funktionen
+- Anwendungen
+
+### Variablen
+Variablen sind Zeichenketten, die mit einem Kleinbuchstaben beginnen. Sie können Buchstaben, Zahlen und Unterstriche enthalten.
+
+### Code
+\`\`\`
+x
+y
+z
+\`\`\`
+`
+
 function Tutorial() {
 
     return (
         <div className="tutorial">
-            <h1>Lambo</h1>
-            <p>
-                Willkommen im Online Interpreter der Programmiersprache Lambo. Lambo ist eine minimalistische Sprache, die auf den Prinzipien des Lambda-Kalküls basiert. Der Lambda-Kalkül ist ein formales System in der mathematischen Logik und der theoretischen Informatik und bildet die Grundlage für funktionale Programmiersprachen. Durch die Nutzung von Funktionen als Obejkte erster Ordung ermöglicht der Lambda-Kalkül eine sehr abstrakte und klare Form der Programmierung.
-            </p>
-            <p>
-                Diese Website ist das Ergebnis einer Seminararbeit aus dem Seminar Funktionale Programmierung an der Universität Hamburg. Sie dient als praktisches Werkzeug, um die Konzepte des Lambda-Kalküls zu erlernen und zu experimentieren, indem man die Lambo-Programmiersprache verwendet.
-            </p>
-            <p>
-                Beginnen Sie mit dem Schreiben von Lambo-Code im Hauptfenster und nutzen Sie die Auswertungsfunktionen, um das Ergebnis zu sehen. Experimentieren Sie mit verschiedenen Funktionen und Parametern, um ein tieferes Verständnis des Lambda-Kalküls zu erlangen.
-            </p>
+            <Markdown>
+                {markdown}
+            </Markdown>
         </div>
     );
 }
